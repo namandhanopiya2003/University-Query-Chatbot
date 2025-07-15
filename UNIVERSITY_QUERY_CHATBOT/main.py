9f822a51da8198af600daf12fef26d1ceeb87f2d
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-15 21:17:07 +0530

    Update documentation

--- UNIVERSITY_QUERY_CHATBOT/main.py
+++ UNIVERSITY_QUERY_CHATBOT/main.py
@@ -4,18 +4,23 @@ import nltk
 import joblib
 import numpy as np
 
+# Download NLTK tokenizer model
 nltk.download('punkt')
 
+# Loads chatbot data (intents and responses)
 with open('data/intents.json', 'r', encoding='utf-8') as file:
     intents = json.load(file)
 
+# Loads trained ML files (created while training model)
 model = joblib.load('chatbot/model.pkl')
 vectorizer = joblib.load('chatbot/vectorizer.pkl')
 label_encoder = joblib.load('chatbot/label_encoder.pkl')
 
+# Text cleaning is done
 def preprocess_text(text):
     return text.lower()
 
+# Finding the intent of the message
 def predict_intent(text):
     text = preprocess_text(text)
     X = vectorizer.transform([text])
@@ -25,12 +30,14 @@ def predict_intent(text):
     confidence = probs[max_idx]
     return intent, confidence
 
+# Picksd one response based on intent
 def get_response(intent):
     for item in intents['intents']:
         if item['tag'] == intent:
             return random.choice(item['responses'])
     return "Sorry, I didn't understand that."
 
+# Understands the user input and give the best reply
 def chatbot_response(text):
     intent, confidence = predict_intent(text)
     if confidence > 0.1:
@@ -38,6 +45,7 @@ def chatbot_response(text):
     else:
         return "Sorry, I didn't understand that."
 
+# Running chatbot
 def main():
     print("Chatbot is running! (type 'quit' or 'exit' to stop)")
     while True:
