1db236f1d2e7c2350fa1f6df181d0d0fd8812d92
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-15 21:21:51 +0530

    Update documentation

--- UNIVERSITY_QUERY_CHATBOT/chatbot/dialogue_manager.py
+++ UNIVERSITY_QUERY_CHATBOT/chatbot/dialogue_manager.py
@@ -1,11 +1,21 @@
 def get_next_action(intent, context, entities):
+
+    # If user said hello
     if intent == 'greeting':
         return 'greeting'
+
+    # If user said goodbye
     elif intent == 'goodbye':
         return 'goodbye'
+
+    # If user is asking about office or working hours
     elif intent == 'ask_hours':
         return 'ask_hours'
+
+    # If user wants to know where the library is
     elif intent == 'ask_library_location':
         return 'ask_library_location'
+
+    # If the intent is something else or unknown
     else:
         return 'default_response'
