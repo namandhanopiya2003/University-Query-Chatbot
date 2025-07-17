e1f14c3d7413da2ed77ffccc378deed384c2ca76
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-17 15:29:05 +0530

    Improved code readability

--- UNIVERSITY_QUERY_CHATBOT/chatbot/logger.py
+++ UNIVERSITY_QUERY_CHATBOT/chatbot/logger.py
@@ -1,8 +1,10 @@
 import logging
 
+# Sets up the log file and log format and saves all chat logs into 'chatbot.log'
 logging.basicConfig(filename='chatbot.log', level=logging.INFO, 
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
+# Saves what the user said and what the bot replied into the log file
 def log_conversation(user_input, bot_response):
     logging.info(f'User: {user_input}')
     logging.info(f'Bot: {bot_response}')
