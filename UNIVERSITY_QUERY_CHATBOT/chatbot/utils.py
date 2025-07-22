b2bf7ee9ab74f7e7ba3514e98ac9c4c60186f2fd
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-22 18:08:10 +0530

    Improved code readability

--- UNIVERSITY_QUERY_CHATBOT/chatbot/utils.py
+++ UNIVERSITY_QUERY_CHATBOT/chatbot/utils.py
@@ -1,10 +1,12 @@
 import random
 import re
 
+# Converts text to lowercase and remove special characters
 def clean_text(text):
-    text = text.lower()
-    text = re.sub(r'[^a-z0-9\s]', '', text)
+    text = text.lower()                                  # Makes everything lowercase
+    text = re.sub(r'[^a-z0-9\s]', '', text)              # Keeps only letters, numbers, and spaces
     return text
 
+# Picks a random item from a list
 def random_choice(choices):
     return random.choice(choices)
