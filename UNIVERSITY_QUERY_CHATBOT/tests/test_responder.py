b334e1edb3df06c7ad97d5c0e8c41dc3b890df91
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-26 21:03:47 +0530

    Added comments

--- UNIVERSITY_QUERY_CHATBOT/tests/test_responder.py
+++ UNIVERSITY_QUERY_CHATBOT/tests/test_responder.py
@@ -1,15 +1,20 @@
 import unittest
 from chatbot.responder import respond
 
+# This class tests if the responder function gives correct replies
 class TestResponder(unittest.TestCase):
-    def test_respond_known_action(self):
-        response = respond('greeting')
-        self.assertIsInstance(response, str)
-        self.assertNotEqual(response, '')
 
+    # Tests if respond() returns a valid reply for a known intent
+    def test_respond_known_action(self):
+        
+        response = respond('greeting')                          # Try a known tag
+        self.assertIsInstance(response, str)                    # The response should be a string
+        self.assertNotEqual(response, '')                       # Response should not be empty
+
+    # Tests if respond() returns the default message for unknown intents
     def test_respond_unknown_action(self):
-        response = respond('unknown_action')
-        self.assertEqual(response, "Sorry, I don't understand.")
+        response = respond('unknown_action')                           # Try an unknown tag
+        self.assertEqual(response, "Sorry, I don't understand.")       # Should return default/fallback response
 
 if __name__ == '__main__':
     unittest.main()
